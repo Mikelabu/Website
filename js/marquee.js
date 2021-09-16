fe974e106f19600b35f07e8fa7665f6b44e423ff
Mikelabu <michael.labu@gmail.com>
2021-09-16 20:14:28 +1200

    added marquee

--- js/marquee.js
+++ js/marquee.js
@@ -1,19 +1,19 @@
 (function(window, document, undefined) {
-    var spaceinterval = 1;
-    var timeinterval = 10; // `speed`
-    var max;
-    var firstrun = true;
+    let spaceinterval = 2;
+    let timeinterval = 20; // `speed`
+   let max;
+  let firstrun = true;
     // Interval function:
-    var gallery = function() {
-     var elem = document.getElementById("marquee-container");
+   let gallery = function() {
+     let elem = document.getElementById("marquee-container");
      if (elem) {
       if (firstrun) {
        max = elem.scrollWidth;
        // Clone the children of the container until the
        // scrollWidth is at least twice as large as max.
-       while (elem.scrollWidth < max * 2) {
-        var length = elem.children.length;
-        for (var i = 0; i < length; ++i) {
+       while (elem.scrollWidth < max * 50) {
+       let length = elem.children.length;
+        for (var i = 0; i <22; ++i) {
          elem.appendChild(elem.children[i].cloneNode(true));
         }
         break;
